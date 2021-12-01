0b39ff8581a964a4d117093c811aa92ea5db0647
Lau Sheng Cher <garylau@live.com>
2021-12-02 01:02:27 +0800

    Resolve conflicts

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.get("/", (req, res) => {
 	let variableA = "123";
 
-<<<<<<< HEAD
-	res.render("about-us", { variableA: variableA });
-=======
 	res.render("index", { variableA: variableA });
->>>>>>> 341915cf133c88eace7febf191845d24fd5b2341
 });
 
 app.use("/product", productRouter);
