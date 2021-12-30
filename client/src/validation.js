19dc3d70bcbbf7b88fa56079c533b97c61fba373
allison4600 <allison4600@gmail.com>
2021-12-30 22:58:22 +0800

    validation

--- client/src/validation.js
+++ client/src/validation.js
@@ -1,25 +1,29 @@
 const form = document.getElementById("form");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
 const email = document.getElementById("email");
 const pass = document.getElementById("password");
+const invalidname = document.getElementById("invalidname");
 const invalidemail = document.getElementById("invalidemail");
 const invalidpass = document.getElementById("invalidpass");
 var emailpattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-console.log(email);
-form.addEventListener('submit', (e) => {
+
+form.addEventListener("input", (e) => {
     var message = [];
 
-    if(email.value == null || pass.value == null) {
+    // if(email.value == null || pass.value == null) {
     
-    }
-    e.preventDefault();
+    // }
 
-    if(!email.value.match(emailpattern)) {
+    if(email.value.match(emailpattern)) {
+        message.getElementById("invalidemail").style.display = "none";
+    } else {
         message.getElementById("invalidemail").style.display = "block";
     }
 
-    if(pass == null || pass == " ") {
-        message.getElementById("invalidpass").style.display = "block";
-    }
+    // if(pass == null || pass == " ") {
+    //     message.getElementById("invalidpass").style.display = "block";
+    // }
 
     if(message.length > 0) {
         e.preventDefault();
